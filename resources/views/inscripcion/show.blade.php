0a92ed97959b5898806042b2530509f6a7f39650
Diossmer <Diossmer@gmail.com>
2020-01-22 23:32:30 -0400

    falta PDF

--- resources/views/inscripcion/show.blade.php
+++ resources/views/inscripcion/show.blade.php
@@ -5,19 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../public/css/pdf.css"> <!--PDF ACEPTA EL LENGUAJE ANTIGUO-->
-    <title>Documento USUARIO</title>
+    <title>Archivo Usuario</title>
 </head>
 <body>
 
 
 
-        @if($seccion)
-        <table>
-		<tr>
-					{{$seccion[1]}}
-		</tr>
-        </table>
-        @endif
+
 </body>
 </html>
 
